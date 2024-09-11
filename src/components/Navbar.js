6b5d3294758b0afc73cd052f309c32bcf87b2ced
raza <razahussainkandhro260@gmail.com>
2024-09-11 21:38:28 +0500

    second portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
 
   const sideList = () => (
     <Box className={classes.menuSliderContainer} component="div">
-      <Avatar className={classes.avatar} src={avatar} alt="Mahmudul Alam" />
+      <Avatar className={classes.avatar} src={avatar} alt="raza hussain" />
       <Divider />
       <List>
         {menuItems.map((item, i) => (
